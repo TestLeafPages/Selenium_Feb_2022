a64e3cd58d6b7a945b31d2d09f813c19c9b97e05
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-02-19 13:20:19 +0530

    Week 1 Day 1 Code

---
+++ src/main/java/week1/day1/LearnMethods.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearnMethods {
+public void printName() {
+	System.out.println("Testleaf");
+	
+}
+public long getMobileNumber() {
+	long number = 9876543210L;
+	return number;
+}
+
+public String getName() {
+	return "Testleaf";
+}
+
+private int addTwoNumbers(int a, int b) {
+	System.out.println(a+b);
+	return a+b;
+}
+
+
+
+}
---
+++ src/main/java/week1/day1/Mobile.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Mobile {
+// syntax: dataType variable = value;
+	public short price = 30000;
+	private int imei = 784658489;
+	long mobileNumber = 9876543210L;
+	
+	float androidVersion = 10.009F;
+	double browserVersion = 987.99883272388;
+	
+	char version = 'X';
+	boolean isMobileSwitchedOn = false;
+	
+	String name = "Testleaf";
+	
+}
